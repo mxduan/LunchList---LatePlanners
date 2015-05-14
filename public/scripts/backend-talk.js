c143d310553b313b7da96c20bb026b5291212264
Kjetil Pettersen <kpettersen@factset.com>
2015-05-14 12:55:06 -0700

    added functions to interact with backend, updated packages files

---
+++ public/scripts/backend-talk.js
@@ -0,0 +1,32 @@
+var addSublist = function(listName, success, error) {
+  $.ajax({
+    method: 'POST',
+    url: '/list/',
+    data: {
+      name: listName
+    },
+    success: success,
+    error: error
+  });
+};
+
+var addToList = function(listId, entryName, success, error) {
+  $.ajax({
+    method: 'POST',
+    url: 'list/' + listId,
+    data: {
+      name: entryName
+    },
+    success: sucess,
+    error: error
+  });
+};
+
+var getList = function() {
+  $.ajax({
+    method: 'GET',
+    url: '/list',
+    success: success,
+    error: error
+  });
+};
