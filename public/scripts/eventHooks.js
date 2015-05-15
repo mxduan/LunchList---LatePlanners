768a9a9b4b38d8364ecc5cbd0381c91ad8b9b459
snowp <snowp@fastmail.fm>
2015-05-14 23:56:57 -0700

    added name bar

--- public/scripts/eventHooks.js
+++ public/scripts/eventHooks.js
@@ -1,5 +1,4 @@
-var inputFieldSetup = function(inputFieldObject, fetchAndLoad)
-{
+var inputFieldSetup = function(inputFieldObject, fetchAndLoad) {
     inputFieldObject.keyup(function(event) {
         if(event.keyCode == 13) {
             event.preventDefault();
@@ -13,7 +12,8 @@ var inputFieldSetup = function(inputFieldObject, fetchAndLoad)
 var joinButtonSetup = function(pushButtonFieldObject, fetchAndLoad)  {
     pushButtonFieldObject.click(function(event) {
         event.preventDefault();
-        addToSublist(pushButtonFieldObject.attr('id'), 'me');
+        
+        addToSublist(pushButtonFieldObject.attr('id'), $('#set-name').val());
         fetchAndLoad();
     });
 };
